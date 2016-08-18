a843539b835d11b933ef7ea5377c0649936679b8
Hugo Alves <goninja@gmail.com>
2016-08-18 16:39:54 +0100

    with bairro on recos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160818143756) do
+ActiveRecord::Schema.define(version: 20160818153837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 20160818143756) do
     t.string   "day_time"
     t.string   "photo"
     t.string   "weather_type"
+    t.string   "bairro"
   end
 
   create_table "users", force: :cascade do |t|
