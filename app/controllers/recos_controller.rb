2176a13ff817bead7ad1f0a5b55184fae6b0e4a3
tauan lemos <tauanmiguel@gmail.com>
2016-08-22 12:58:31 +0100

    added map to home

--- app/controllers/recos_controller.rb
+++ app/controllers/recos_controller.rb
@@ -1,6 +1,6 @@
 class RecosController < ApplicationController
   before_action :set_reco, only: [:show, :edit, :update, :destroy]
-
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
   # GET /recos
   # GET /recos.json
   def index
