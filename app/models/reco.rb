ba2c02990f12731b46cb195d63b8f7927070b8dc
Hugo Alves <goninja@gmail.com>
2016-08-22 18:00:20 +0100

    com novos cards

--- app/models/reco.rb
+++ app/models/reco.rb
@@ -2,7 +2,7 @@ class Reco < ActiveRecord::Base
   mount_uploader :photo, PhotoUploader
   acts_as_commontable
   acts_as_votable
-  default_scope { order(:cached_votes_up => :desc) }
+  default_scope { order(:cached_votes_up => :desc, :cached_votes_down => :asc) }
   belongs_to :user
   # after_create :send_reco
 
