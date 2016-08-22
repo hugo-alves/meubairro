97727612bf04984b55b5d8225bbd9366d05ccdc1
tauan lemos <tauanmiguel@gmail.com>
2016-08-22 12:02:20 +0100

    Navbar signup link

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   devise_for :users
   mount Commontator::Engine => '/commontator'
 
-  root to: 'recos#index'
+  root to: 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
