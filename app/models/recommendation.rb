ea9ee495067d12bf00e21bb093415d195ffe2161
tauan lemos <tauanmiguel@gmail.com>
2016-08-22 17:10:53 +0100

    added geocode

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -3,6 +3,8 @@ class Recommendation < ActiveRecord::Base
   acts_as_commontable
   acts_as_votable
   belongs_to :user
+  geocoded_by :address   # can also be an IP address
+  after_validation :geocode
   # after_create :send_reco
 
   private
